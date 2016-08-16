74f464b7139748f961e9a0ad9d9223b999b85d73
nkgautam <narendra.gautam1@gmail.com>
2016-08-16 11:03:48 +0530

    Initial Commit

---
+++ Default.aspx
@@ -0,0 +1,96 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+    <script src="Scripts/jquery-1.9.1.min.js"></script>
+    <script src="Scripts/BrowserWindowSize.js"></script>
+    <script type = "text/javascript">
+        function GetTextboxCordinates() {
+            var txt = document.getElementById("btnscreenshot");
+            var p = GetScreenCordinates(txt);
+            $('#<%= ScreenCordinates.ClientID %>').val(p);
+        }
+    </script>
+
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:HiddenField ID="ScreenCordinates" runat="server" />
+        <table>
+        <%--<tr>
+        <td><b>Enter Web Url:</b></td>
+        <td><asp:TextBox ID="txtweburl" runat="server" /></td>
+        </tr>--%>
+        <tr>
+        <td></td>
+        <td>
+        <asp:Button ID="btnscreenshot" Text="Take Screenshot" runat="server" OnClick="btnscreenshot_click" />
+        </td>
+        </tr>
+        </table>
+        <br />
+        <asp:Image ID="imgscreenshot" runat="server" Visible = "false"  />
+        <h3>
+            herer eh <br />
+            rerwer<br />
+            sfdsfs<br />
+            fs<br />
+            fs<br />
+            fsf<br />
+            sf<br />
+            sf<br />
+            sfs<br />
+            fs<br />
+            fs<br />
+            fs<br />
+            fs<br />
+            f<br />
+            sf<br />
+            sf<br />
+            sf<br />
+            sf<br />
+            sf<br />
+            sf<br />
+            sf<br />
+            s<br />
+            fs<br />
+            fs<br />
+            fs<br />
+            fs<br />
+            f<br />
+            sfv
+            sf<br />
+            sf<br />
+            sf<br /><br />
+            sf<br />
+            <br />
+            fs
+            fs<br />v
+            f<br />
+            sf<br />
+            s<br />
+            fs<br />
+            f<br />
+            sf<br />
+            sf<br />
+            s<br />
+            f<br />
+            sfs<br />
+            f<br />
+            sf<br />
+            sf<br />
+            sf<br />
+            sf<br />
+            s<br />
+            ffsdsfs<br />fsf
+            sv<br />
+            end<br />
+        </h3>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+    }
+
+    protected void btnscreenshot_click(object sender, EventArgs e)
+    {
+        //Thread thread = new Thread(GenerateThumbnail);
+        //thread.SetApartmentState(ApartmentState.STA);
+        //thread.Start();
+        //thread.Join();
+        
+        Bitmap bitmap = new Bitmap(Convert.ToInt32(Session["BrowserWidth"]), Convert.ToInt32(Session["BrowserHeight"]));
+        Graphics graphics = Graphics.FromImage(bitmap as System.Drawing.Image);
+
+        graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+        MemoryStream stream = new MemoryStream();
+        bitmap.Save(@"D:\myscreenshot.jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        bitmap.Save(stream, ImageFormat.Jpeg);
+        byte[] strbytes = stream.ToArray();
+
+        imgscreenshot.Visible = true;
+        // imgscreenshot.Width = webrowse.Width;
+        // imgscreenshot.Height = webrowse.Height;
+
+        imgscreenshot.ImageUrl = "data:image/jpeg;base64," + Convert.ToBase64String(strbytes);
+    }
+    private void GenerateThumbnail()
+    {
+        WebBrowser webrowse = new WebBrowser();
+        webrowse.ScrollBarsEnabled = true;
+        webrowse.AllowNavigation = true;
+        String urls = Request.Url.ToString();
+       // webrowse.Navigate(txtweburl.Text.Trim());
+        //webrowse.Width = Convert.ToInt32(Session["BrowserWidth"]);
+        //webrowse.Height = Convert.ToInt32(Session["BrowserHeight"]);
+        webrowse.DocumentCompleted += webbrowse_DocumentCompleted;
+        
+        while (webrowse.ReadyState != WebBrowserReadyState.Complete)
+        {
+            System.Windows.Forms.Application.DoEvents();
+        }
+    }
+    private void webbrowse_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+    {
+        //WebBrowser webrowse = sender as WebBrowser;
+        //Bitmap bitmap = new Bitmap(webrowse.Width, webrowse.Height);
+        //webrowse.DrawToBitmap(bitmap, webrowse.Bounds);
+        //MemoryStream stream = new MemoryStream();
+        //bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+        //bitmap.Save(@"D:\myscreenshot.jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        //byte[] strbytes = stream.ToArray();
+
+        Bitmap bitmap = new Bitmap(Convert.ToInt32(Session["BrowserWidth"]), Convert.ToInt32(Session["BrowserHeight"]));
+        Graphics graphics = Graphics.FromImage(bitmap as System.Drawing.Image);
+
+        graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+        MemoryStream stream = new MemoryStream();
+        bitmap.Save(@"D:\myscreenshot.jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        bitmap.Save(stream, ImageFormat.Jpeg);
+        byte[] strbytes = stream.ToArray();
+
+        imgscreenshot.Visible = true;
+       // imgscreenshot.Width = webrowse.Width;
+       // imgscreenshot.Height = webrowse.Height;
+
+        imgscreenshot.ImageUrl = "data:image/jpeg;base64," + Convert.ToBase64String(strbytes);
+    }
+}
